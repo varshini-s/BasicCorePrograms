664e6f3be751e13f42b21acc5c7aa2f9ecf289ce
varshini <dreamvarshini@gmail.com>
2021-08-22 15:37:25 +0530

    ADDED:Vowel or Consonant Program

---
+++ src/com/bridgelabz/basiccoreprograms/VowelOrConsonant.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class VowelOrConsonant 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		char alphabet='t';
+		
+		if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+		{
+			System.out.println("Given Aplbhabet :"+alphabet+" is a vowel");
+		}
+		else
+		{
+			System.out.println("Given Aplbhabet :"+alphabet+" is a Consonant");
+
+		}
+
+	}
+
+}
