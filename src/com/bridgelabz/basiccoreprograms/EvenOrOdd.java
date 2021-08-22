a1ffdd7f31a944da869282221af066c9ac433762
varshini <dreamvarshini@gmail.com>
2021-08-22 15:33:42 +0530

    ADDED:Even or ODD Program

---
+++ src/com/bridgelabz/basiccoreprograms/EvenOrOdd.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class EvenOrOdd 
+{
+
+	public static void main(String[] args) {
+		
+		int number=21;
+		
+		if(number%2==0)
+		{
+			System.out.println("Given Number:"+number+" is even");
+		}
+		else
+		{
+			System.out.println("Given Number:"+number+" is odd");
+		}
+
+	}
+
+}
