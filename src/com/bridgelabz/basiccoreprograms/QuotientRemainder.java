06fa26a76c78971578ced8eccf15efb8d9e81ea7
varshini <dreamvarshini@gmail.com>
2021-08-22 15:36:33 +0530

    ADDED:Quotient and Remainder Program

---
+++ src/com/bridgelabz/basiccoreprograms/QuotientRemainder.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.basiccoreprograms;
+
+public class QuotientRemainder {
+	
+	public static void main(String args[]) 
+	{
+		//variables
+		int number1=20;
+		int number2=3;
+		
+		int quotient,remainder;
+		quotient=number1/number2;
+		remainder=number1%number2;
+		System.out.println("Quotient of "+number1+" divided by "+number2 +" is "+quotient);
+		System.out.println("remainder of "+number1+" divided by "+number2 +" is "+remainder);
+
+	}
+
+}
